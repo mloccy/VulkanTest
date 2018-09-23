cc1fc0815d5588184cb9d08b137149b6b1b9250b
Colm Vize <colm@artomatix.com>
2018-09-23 10:57:14 +0100

    fix tabs

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,26 @@
 #include <iostream>
 int main(int argc, char* argv[])
 {
-	glfwInit();
+    glfwInit();
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-	std::cout << extensionCount << " extensions supported" << std::endl;
+    std::cout << extensionCount << " extensions supported" << std::endl;
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 
-	glfwDestroyWindow(window);
+    glfwDestroyWindow(window);
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
